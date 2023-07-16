028c84cec8a5c54ba7684b6af5147ba727ad540d
Aram Valcourt <115132116+AramCodes@users.noreply.github.com>
2023-07-16 00:26:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,92 @@
-const searchField = document.getElementById("searchField");
 const searchBtn = document.getElementById("searchBtn");
 const listContainer = document.getElementById("listContainer");
 
-require('dotenv').config()
-const api_key = process.env.API_KEY;
-const url = `http://www.omdbapi.com/?apikey=${api_key}&`;
+searchBtn.addEventListener("click", findSearch)
 
-//make search button populate found movies
+function findSearch() {
+    const searchField = document.getElementById("searchField");
+    getMovies(searchField.value);
+}
 
-searchBtn.addEventListener("click", getMovies)
+function getMovies(searchParameter) {
 
-function getMovies() {
+    const url = `http://www.omdbapi.com/?apikey=8e736192&s=${encodeURIComponent(searchParameter)}`;
+
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        // handle the response
+        listContainer.innerHTML = "";
+        let items = data.Search;
+        for (let item of items) {
+            let movieId = item.imdbID;
+            movieCollection(movieId);
+        }
     })
-    .catch(error => {
-        // handle the error
-    });
+    .catch(error => console.log(error));
+}
+
+function movieCollection(movieId) {
+
+    const url = `https://www.omdbapi.com/?apikey=8e736192&i=${movieId}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            listContainer.innerHTML += `             
+            <div class="card">
+            <img src="${data.Poster}" alt="movie-thumbnail" class="card-thumbnail">
+
+            <div class="card-details">
+                <div class="card-details-first-row">
+                    <h3 class="card-title">${data.Title}</h3>
+
+                    <img src="images/star.svg" alt="star-rating" class="stars">
+
+                    <p class="rating">${data.Ratings[0].Value}</p>
+                </div>
+
+                <div class="card-details-second-row">
+                    <p class="movie-runtime">${data.Runtime}</p>
+                    <p class="movie-genre">${data.Genre}</p>
+                    <p class="add-watchlist">
+                        <i class="fa-solid fa-circle-plus white btns"></i>
+                        Watchlist
+                    </p>
+                </div>
+
+                <div class="card-details-third-row">
+                    <p class="movie-synopsis">${data.Plot}</p>
+                </div>
+
+                    
+            </div>
+        </div>`
+        });
+}
+
+//check
+
+listContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("fa-circle-plus")) {
+        const movieData = event.target.closest("card-details"); 
+        const movieId = movieData.dataset.movieId;
+        addToWatchlist(movieId);
+    }
+});
+
+function addToWatchlist(movieId) {
+    let watchlist = getWatchlistFromStorage();
+    if (watchlist.includes(movieId)) {
+        alert("Movie is already in the watchlist.");
+        return;
+    }
+
+    watchlist.push(movieId);
+    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    alert("Movie added to watchlist.");
+}
+
+function getWatchlistFromStorage() {
+    const watchlistData = localStorage.getItem("watchlist");
+    return watchlistData ? JSON.parse(watchlistData) : [];
 }
---
+++ watchlist.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let moviesDisplay = document.getElementById("listContainer");
+    let message = document.getElementById("center");
+    let watchlist = getWatchlistFromStorage();
+  
+    if (watchlist.length > 0) {
+      message.style.display = "none";
+      for (let movieId of watchlist) {
+        fetchMovieData(movieId);
+      }
+    }
+  
+    function fetchMovieData(movieId) {
+      const url = `https://www.omdbapi.com/?apikey=49653bc0&i=${movieId}`;
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          moviesDisplay.innerHTML += `
+        <div class="card">
+            <img src="${data.Poster}" alt="movie-thumbnail" class="card-thumbnail">
+
+            <div class="card-details">
+                <div class="card-details-first-row">
+                    <h3 class="card-title">${data.Title}</h3>
+
+                    <img src="images/star.svg" alt="star-rating" class="stars">
+
+                    <p class="rating">${data.Ratings[0].Value}</p>
+                </div>
+
+                <div class="card-details-second-row">
+                    <p class="movie-runtime">${data.Runtime}</p>
+                    <p class="movie-genre">${data.Genre}</p>
+                    <p class="add-watchlist">
+                        <i class="fa-solid fa-circle-plus white btns"></i>
+                      Watchlist
+                    </p>
+                </div>
+
+                <div class="card-details-third-row">
+                    <p class="movie-synopsis">${data.Plot}</p>
+                </div>    
+            </div>
+        </div>
+          `;
+        });
+    }
+  
+    function getWatchlistFromStorage() {
+      const watchlistData = localStorage.getItem("watchlist");
+      return watchlistData ? JSON.parse(watchlistData) : [];
+    }
+  
+    moviesDisplay.addEventListener("click", function (event) {
+      if (event.target.id === "remove-icon") {
+        const movieData = event.target.closest("#movie-data");
+        const movieId = movieData.getAttribute("data-movie-id");
+        removeFromWatchlist(movieId);
+        movieData.remove(); // Remove the movie data from the watchlist display
+      }
+    });
+  
+    function removeFromWatchlist(movieId) {
+      let watchlist = getWatchlistFromStorage();
+      const index = watchlist.indexOf(movieId);
+      if (index !== -1) {
+        watchlist.splice(index, 1);
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+        alert("Movie removed from watchlist.");
+      }
+    }
+  });
